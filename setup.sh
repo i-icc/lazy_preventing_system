6789608755c1f7aeb3a97788a0cd368e28cd5780
新田一斗 <nittaichito@shintaichitonoMacBook-Pro.local>
2021-11-13 19:04:08 +0900

    remove yolo

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get -y install python3-dev
-sudo apt-get -y install python3-pip
 sudo pip3 install --upgrade pip
 sudo apt-get install libavutil56 libcairo-gobject2 libgtk-3-0 libqtgui4 libpango-1.0-0 libqtcore4 libavcodec58 libcairo2 libswscale5 libtiff5 libqt4-test libatk1.0-0 libavformat58 libgdk-pixbuf2.0-0 libilmbase23 libjasper1 libopenexr23 libpangocairo-1.0-0 libwebp6
+sudo pip3 install opencv-python==4.1.0.25
 
-sudo pip3 install -r yolov5/requirements.txt
