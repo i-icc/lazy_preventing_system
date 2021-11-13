c8ece2abb20866ec7545c69af81750ff0de4d7d1
新田一斗 <nittaichito@shintaichitonoMacBook-Pro.local>
2021-11-13 15:54:53 +0900

    load camera

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import cv2
 import numpy as np
 
 def main():
+    cam = cv2.VideoCapture(0)
+    if not cam.isOpened():
+        print("The camera could not be loaded.")
+        return
     print("test")
 
 if __name__ == "__main__":
