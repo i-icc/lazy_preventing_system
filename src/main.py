0495a849ad511e4e6aaef69d79291967e43b54bc
新田一斗 <nittaichito@shintaichitonoMacBook-Pro.local>
2021-11-13 17:35:28 +0900

    read image from camera

--- src/main.py
+++ src/main.py
@@ -2,11 +2,19 @@ import cv2
 import numpy as np
 
 def main():
+    # load camera
     cam = cv2.VideoCapture(0)
     if not cam.isOpened():
         print("The camera could not be loaded.")
         return
-    print("test")
+
+    # get Image
+    while True:
+        ret, frame = cam.read()
+        cv2.imshow("test", frame)
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
+    
 
 if __name__ == "__main__":
     main()
