1bc055f7884524d7ca7edda682c72fe40cc50e95
新田一斗 <nittaichito@shintaichitonoMacBook-Pro.local>
2021-11-13 18:06:43 +0900

    オプションでカメラを指定できるようにした

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
+from argparse import ArgumentParser
 
 def main():
+    args = get_args()
     # load camera
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(args.camera_number)
     if not cam.isOpened():
         print("The camera could not be loaded.")
         return
@@ -15,6 +17,17 @@ def main():
         cv2.waitKey(1)
         cv2.destroyAllWindows()
     
+def get_args():
+    parser = ArgumentParser(description="lazy_preventing_system")
+    parser.add_argument(
+        "-cam",
+        "--camera_number",
+        type=int,
+        default=0,
+        help="set camera number.default=0",
+    )
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
     main()
