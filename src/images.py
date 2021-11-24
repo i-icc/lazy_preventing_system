2896dedc7d8568a5b6eb6ae2b00590654cd060e2
新田一斗 <shintaichito@dot1xs3715.14-192-19.wlan.ms.dendai.ac.jp>
2021-11-24 12:47:45 +0900

    create test

--- src/images.py
+++ src/images.py
@@ -18,14 +18,23 @@ class Images:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             p1 , p2 = 20, 20
             hlower = self.cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2, minSize=(p1, p2))
-            # 顔領域を赤色の矩形で囲む
             for (x, y, w, h) in hlower:
                 cv2.rectangle(frame, (x, y), (x + w, y+h), (255,0,0), 3)
             cv2.imshow("test", frame)
             cv2.waitKey(1)
             cv2.destroyAllWindows()
 
-    def human_perception(self):
-        image = self.get_image()
+    def human_perception(self,image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        hlower = self.cascade.detectMultiScale(gray)
+        return hlower
 
-        pass
+if __name__ == "__main__":
+    images = Images(0)
+    a = cv2.imread("./test.jpeg")
+    h = images.human_perception(a)
+    for (x, y, w, h) in h:
+        cv2.rectangle(a, (x, y), (x + w, y+h), (255,0,0), 3)
+    cv2.imshow("test", a)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
